e9b96cc308ebc814a89eaeedacdac1a9ed3d8885
ferreramfe <matheuseziquiel@gmail.com>
2022-02-27 22:44:28 -0300

    added route to home

--- dbrain-frontend-test/src/app/app.module.ts
+++ dbrain-frontend-test/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { CoreModule } from "./core/core.module";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -9,8 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
