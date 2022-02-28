e3043e5ea5161d86dbcc15b59d87eb55eef6a945
ferreramfe <matheuseziquiel@gmail.com>
2022-02-28 01:34:48 -0300

    added categories to productlist component

--- dbrain-frontend-test/src/app/pages/home/product-list/product-list.component.ts
+++ dbrain-frontend-test/src/app/pages/home/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../../services/product.service';
 import { Product } from "../../../common/product";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -10,15 +11,32 @@ import { Product } from "../../../common/product";
 export class ProductListComponent implements OnInit {
 
   products?: Product[];
+  categories?: string[];
+  currentCategoryId?: number;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
 
-  ngOnInit(): void {    
-    this.productService.getProductList().subscribe(
-      res => {
-        this.products = res;
-      }
-    )
+  ngOnInit(): void {   
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    }) 
   }
 
+  listProducts() {
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+      this.productService.getProductListByCategoryId(this.currentCategoryId);
+    } else {
+      this.productService.getProductList().subscribe(
+        res => {
+          this.products = res;
+          console.log(res);
+          this.categories = [ ...new Set(this.products?.map(product => product.category)) ] as string[]
+        }
+      )
+    } 
+  }
 }
