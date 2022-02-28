5389a3cdbe2a890c2b2b2fe298b76b91a554d00c
ferreramfe <matheuseziquiel@gmail.com>
2022-02-27 22:43:00 -0300

    added home page to show products

---
+++ dbrain-frontend-test/src/app/pages/home/product-list/product-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../../services/product.service';
+import { Product } from "../../../common/product";
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products?: Product[];
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {    
+    this.productService.getProductList().subscribe(
+      res => {
+        this.products = res;
+      }
+    )
+  }
+
+}
