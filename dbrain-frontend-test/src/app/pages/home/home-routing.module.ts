0d0f1483bf03adacbffb0d5d992b90524acd4508
ferreramfe <matheuseziquiel@gmail.com>
2022-03-01 11:20:00 -0300

    configured routes for product details

--- dbrain-frontend-test/src/app/pages/home/home-routing.module.ts
+++ dbrain-frontend-test/src/app/pages/home/home-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailComponent } from 'src/app/components/product-detail/product-detail.component';
 import { ProductListComponent } from '../../components/product-list/product-list.component';
 
 const routes: Routes = [
+  { path: 'products/:id', component: ProductDetailComponent },
   { path: 'search/:keyword', component: ProductListComponent },
   { path: 'category/:categoryName', component: ProductListComponent },
   { path: '', component: ProductListComponent },
