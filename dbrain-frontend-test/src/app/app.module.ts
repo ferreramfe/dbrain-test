70f751fb107346db571f96fee9d481444244f302
ferreramfe <matheuseziquiel@gmail.com>
2022-03-01 01:35:04 -0300

    added search components to project

--- dbrain-frontend-test/src/app/app.module.ts
+++ dbrain-frontend-test/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { CoreModule } from "./core/core.module";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductCategoryListComponent } from './components/product-category-list/product-category-list.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductCategoryListComponent
+    ProductCategoryListComponent,
+    SearchComponent
   ],
   imports: [
     AppRoutingModule,
--- dbrain-frontend-test/src/app/pages/home/home-routing.module.ts
+++ dbrain-frontend-test/src/app/pages/home/home-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from '../../components/product-list/product-list.component';
 
 const routes: Routes = [
+  { path: 'search/:keyword', component: ProductListComponent },
   { path: 'category/:categoryName', component: ProductListComponent },
   { path: '', component: ProductListComponent },
   { path: '', redirectTo: '/', pathMatch: 'full' },
